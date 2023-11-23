9d93a2f720f5336b95bca91de0cb75f1ec10c234
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-23 13:38:05 +0530

    added contact info

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,4 +1,14 @@
 public class AddressBookSystem {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    Long phoneNumber;
+    String email;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
     }
